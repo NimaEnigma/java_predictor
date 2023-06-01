fba160671d99238ce8b929e2c51cb4970f35b427
Nima Miri <1381.n.m@gmail.com>
2023-06-01 17:21:25 +0330

    hello there

--- Predictor/src/main/java/hardwar/branch/prediction/judged/PAp/PAp.java
+++ Predictor/src/main/java/hardwar/branch/prediction/judged/PAp/PAp.java
@@ -25,25 +25,50 @@ public class PAp implements BranchPredictor {
         this.branchInstructionSize = branchInstructionSize;
 
         // Initialize the PABHR with the given bhr and branch instruction size
-        PABHR = null;
+        PABHR = new RegisterBank(branchInstructionSize, BHRSize);
 
         // Initializing the PAPHT with BranchInstructionSize as PHT Selector and 2^BHRSize row as each PHT entries
         // number and SCSize as block size
-        PAPHT = null;
+        PAPHT = new PerAddressPredictionHistoryTable(branchInstructionSize , (int)Math.pow(2 , BHRSize) , SCSize);;
 
         // Initialize the SC register
-        SC = null;
+        SC = new SIPORegister("SC" , SCSize , null);
     }
 
     @Override
     public BranchResult predict(BranchInstruction branchInstruction) {
         // TODO: complete Task 1
-        return BranchResult.NOT_TAKEN;
+        ShiftRegister BHR = PABHR.read(branchInstruction.getInstructionAddress());
+
+        Bit[] bI = branchInstruction.getInstructionAddress();
+        Bit[] concat = new Bit[bI.length + BHR.getLength()];
+        System.arraycopy(bI, 0, concat, 0, bI.length);
+        System.arraycopy(BHR.read(), 0, concat, bI.length, BHR.getLength());
+
+        try {
+            SC.load(PAPHT.get(concat));
+            if (SC.read()[0] == Bit.ONE)
+                return BranchResult.TAKEN;
+            return BranchResult.NOT_TAKEN;
+        }
+        catch (Exception e) {
+            SC.load(getDefaultBlock());
+            return BranchResult.NOT_TAKEN;
+        }
     }
 
     @Override
     public void update(BranchInstruction instruction, BranchResult actual) {
         // TODO:complete Task 2
+        SC.load(CombinationalLogic.count(SC.read(), BranchResult.isTaken(actual), CountMode.SATURATING));
+        ShiftRegister BHR = PABHR.read(instruction.getInstructionAddress());
+        Bit[] bI = instruction.getInstructionAddress();
+        Bit[] concat = new Bit[bI.length + BHR.getLength()];
+        System.arraycopy(bI, 0, concat, 0, bI.length);
+        System.arraycopy(BHR.read(), 0, concat, bI.length, BHR.getLength());
+        PAPHT.putIfAbsent(concat, SC.read());
+        BHR.insert(Bit.of(BranchResult.isTaken(actual)));
+        PABHR.write(instruction.getInstructionAddress(), BHR.read());
     }
 
 
